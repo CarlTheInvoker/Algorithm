c5f54f68095479f51091625d33fbc44a9cc532b2
DeepWaterWhale <xiangjun.app@hotmail.com>
2020-10-30 00:02:27 +0800

    Add merge sort.

---
+++ Algorithm/Sort/MergeSortExtension.cs
@@ -0,0 +1,45 @@
+﻿namespace Algorithm.Sort
+{
+    using System;
+    public static class MergeSortExtension
+    {
+        public static void MergeSort<TElement>(this TElement[] elements) where TElement : IComparable<TElement>
+        {
+            MergeSort(elements, new TElement[elements.Length], 0, elements.Length);
+        }
+
+        /// <summary>
+        /// Sort elements[start, end)
+        /// </summary>
+        /// <typeparam name="TElement">Element type</typeparam>
+        /// <param name="elements">The original elements</param>
+        /// <param name="temp">A temp array used when merge</param>
+        /// <param name="start">Start index</param>
+        /// <param name="end">End index</param>
+        private static void MergeSort<TElement>(TElement[] elements, TElement[] temp, int start, int end) where TElement : IComparable<TElement>
+        {
+            if (start == end - 1) return;
+            int middle = (start + end) / 2;
+
+            // Sort sub array
+            MergeSort(elements, temp, start, middle);
+            MergeSort(elements, temp, middle, end);
+
+            // Merge sorted sub array and store the result in temp
+            int left = start, right = middle;
+            for (int i = 0; i < end - start; ++i)
+            {
+                if (right == end) temp[i] = elements[left++];
+                else if (left == middle) temp[i] = elements[right++];
+                else if (elements[left].CompareTo(elements[right]) <= 0) temp[i] = elements[left++];
+                else temp[i] = elements[right++];
+            }
+
+            // Copy temp result back into original array
+            for (int i = 0; i < end - start; ++i)
+            {
+                elements[start + i] = temp[i];
+            }
+        }
+    }
+}
---
+++ AlgorithmTests/Sort/MergeSortExtensionTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithm.Sort;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Tests
+{
+    [TestClass()]
+    public class MergeSortExtensionTest
+    {
+        [TestMethod()]
+        public void SortTest()
+        {
+            int[] nums = new int[] { 1, 2, 3, 4, 4, 3, 2, 1, 1, 3, 2, 4, 2, 4, 3, 1, 0, 0 };
+            nums.MergeSort();
+
+            for (int i = 1; i < nums.Length; ++i)
+            {
+                Assert.IsTrue(nums[i - 1] <= nums[i]);
+            }
+        }
+    }
+}
